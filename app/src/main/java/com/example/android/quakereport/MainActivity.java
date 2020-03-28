cc22afc087ff66a9fa0126a9693ea3c4b7f9cd34
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-03-27 20:04:05 -0600

    Parse JSON response from our app

--- app/src/main/java/com/example/android/quakereport/MainActivity.java
+++ app/src/main/java/com/example/android/quakereport/MainActivity.java
@@ -18,13 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake("5.1","San Francisco, CA", "Feb 2, 2016"));
-        earthquakes.add(new Earthquake("7.0","London", "Mar 22, 2017"));
-        earthquakes.add(new Earthquake("1.0","Italy", "Apr 10, 2014"));
-        earthquakes.add(new Earthquake("2.1","Tokyo", "July 11, 2018"));
-        earthquakes.add(new Earthquake("1.1","Mexico city", "Jan 6 1994"));
-        earthquakes.add(new Earthquake("3.0","Canada", "Dec 10, 2021"));
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
 
         // Find a reference to the {@link ListView} in the layout
