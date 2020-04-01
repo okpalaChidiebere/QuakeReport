ab730a4319fba6c5bd1fbec892fc293c9f652c36
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-03-31 23:00:32 -0600

    Check Network Connectivity Status

--- app/src/main/java/com/example/android/quakereport/MainActivity.java
+++ app/src/main/java/com/example/android/quakereport/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -73,6 +76,17 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             }
         });
 
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        // Initialize the loader. Pass in the int ID constant defined above and pass in null for
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+
         // Get a reference to the LoaderManager, in order to interact with loaders.
         LoaderManager loaderManager = LoaderManager.getInstance(this);
 
@@ -81,6 +95,20 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // because this activity implements the LoaderCallbacks interface).
         Log.i(LOG_TAG, "TEST: initialising loader");
         loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+
+            /* To see a more User intractive way of adding a refresh button or swipeRefresh when there is no internet
+             connection check this link
+             https://github.com/udacity/ud843-QuakeReport/commit/f0f9cd5ee7a8d67bd2e6f7e2539664a95499831b*/
+        }
 
     }
 
